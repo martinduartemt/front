d2900ab14fd1ee2128d6359f11d3e9c08ed846d9
martinduartemt <martinduarteflorez@iMac-de-Martin.local>
2022-09-23 17:57:46 -0500

    organice los archivos de las paginas

---
+++ paginas/clients.js
@@ -0,0 +1,107 @@
+function showQuadbike(){
+  //elemento del DOM->document object model
+  const $responseContainer=document.getElementById("response");
+  // $responseContainer.innerHTML='texto agregado desde javascript';
+  $.ajax({
+      url:"https://ga6ae41cae65926-vogwkjxu2344xt5x.adb.us-phoenix-1.oraclecloudapps.com/ords/admin/quadbike/quadbike",
+      type:"GET",
+      datatype:"JSON",
+      success:function(response){
+          console.log(response)
+          console.log(response.items)
+          for(let x=0;x<response.items.length;x++){
+              const costume= response.items[x];
+              $responseContainer.innerHTML+=`
+              id1:${costume.id}
+              <br>
+              brand1:${costume.brand} 
+              <br>
+              model1:${costume.model} 
+              <br>
+              id_category1:${costume.category_id} 
+              <br>
+              name1:${costume.name} 
+              <br>
+              `;
+          }
+      }
+  })
+}
+
+function createCostume(){
+  
+  let dataToSend={
+      "id": parseInt( $("#id1").val()),
+      "brand":$("#brand1").val(),
+      "model": parseInt($("#model1").val()),
+      "category_id":parseInt($("#id_category1").val()),
+      "name":$("#name1").val(),
+  }
+  dataToSend=JSON.stringify(dataToSend);   
+
+  const settings = {
+      "url": "https://ga6ae41cae65926-vogwkjxu2344xt5x.adb.us-phoenix-1.oraclecloudapps.com/ords/admin/quadbike/quadbike",
+      "method": "POST",        
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "data": dataToSend,        
+    };
+    
+    
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
+
+}
+
+function updateCostume(){
+  
+  let dataToSend={
+    "id": parseInt( $("#id1").val()),
+    "brand":$("#brand1").val(),
+    "model": parseInt($("#model1").val()),
+    "category_id":parseInt($("#id_category1").val()),
+    "name":$("#name1").val(),
+  }
+  dataToSend=JSON.stringify(dataToSend);   
+
+  const settings = {
+      "url": "https://ga6ae41cae65926-vogwkjxu2344xt5x.adb.us-phoenix-1.oraclecloudapps.com/ords/admin/quadbike/quadbike",
+      "method": "PUT",        
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "data": dataToSend,        
+    };      
+    
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
+
+}
+
+function deleteCostume(){
+  
+  let dataToSend={
+      "id": parseInt( $("#id1").val()),        
+  }
+  dataToSend=JSON.stringify(dataToSend);   
+
+  const settings = {
+      "url": "https://ga6ae41cae65926-vogwkjxu2344xt5x.adb.us-phoenix-1.oraclecloudapps.com/ords/admin/quadbike/quadbike",
+      "method": "DELETE",        
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "data": dataToSend,        
+    };      
+    
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
+
+}
+
+
+
